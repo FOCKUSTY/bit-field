b4597c9ef5591ad1950919d5c82badcc298905fc
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2025-06-19 23:59:44 +0500

    -add docs

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # bit-field
+Simple bit field for some systems: rights, permissions, configs and more...
+
+## How to use `BitField` class?
+
+```ts
+import { BitField } from "fbit-field";
+
+// BitField.equals
+// compares two values
+BitField.equals(1n, 1n)  // true
+BitField.equals(1n, "1") // true
+BitField.equals(1n, 2n)  // false
+BitField.equals(1n, "2") // false
+
+// BitField.summarize
+// summurize values
+BitField.summarize([1n, 2n, 4n]) // 7n
+BitField.summarize([2n, 2n, 4n]) // 6n
+BitField.summarize([1n, 4n]) // 5n
+BitField.summarize([1n, 1n]) // 1n
+BitField.summarize(1n) // 1n
+
+// BitField.add
+// summurize values
+BitField.add(1n, [2n, 4n]) // 7n
+BitField.add(2n, [2n, 4n]) // 6n
+BitField.add(1n, 4n) // 5n
+BitField.add(1n, 1n) // 1n
+
+// BitField.remove
+// subtract values
+BitField.remove(7n, [4n, 2n]) // 1n
+BitField.remove(14n, [4n, 2n]) // 8n
+BitField.remove(14n, [4n, 2n, 1n]) // 8n
+BitField.remove(14n, [1n]) // 14n
+
+// BitField.logarithm2
+// takes the logarithm of a number
+BitField.logarithm2(1n << 10n) // 10n
+BitField.logarithm2(1n << MULTIPLIER) // MULTIPLIER
+BitField.logarithm2(2n << 10n) // 11n
+BitField.logarithm2(4n << 10n) // 12n
+
+// BitField.max
+// takes max value of values
+BitField.max(1n, 2n, 3n, 4n) // 4n
+
+// new BitField().add
+// summurize values
+new BitField(1n).add([2n, 4n]) // 7n
+new BitField(2n).add([2n, 4n]) // 6n
+new BitField(1n).add(4n) // 5n
+new BitField(1n).add(1n) // 1n
+
+// new BitField().remove
+// subtract values
+new BitField(7n).remove([4n, 2n]) // 1n
+new BitField(14n).remove([4n, 2n]) // 8n
+new BitField(14n).remove([4n, 2n, 1n]) // 8n
+new BitField(14n).remove([1n]) // 14n
+
+// new BitField().has
+// checking bits equals
+new BitField(1n).has(1n)  // true
+new BitField(1n).has("1") // true
+new BitField(1n).has(2n)  // false
+new BitField(1n).has("2") // false
+```
--- src/bit-field.ts
+++ src/bit-field.ts
@@ -2,22 +2,96 @@ import type { ArrayOrType, Bit } from "./types";
 import { DEFAULT_BIT } from "./constants";
 
 class BitField {
+  /**
+   * compares two values
+   * 
+   * @example
+   * ```ts
+   * BitField.equals(1n, 1n)  // true
+   * BitField.equals(1n, "1") // true
+   * BitField.equals(1n, 2n)  // false
+   * BitField.equals(1n, "2") // false
+   * ```
+   */
   public static equals(first: Bit, second: Bit): boolean {
     return BigInt(first) === BigInt(second);
   }
 
+  /**
+   * summurize values
+   * 
+   * @example
+   * ```ts
+   * BitField.summarize([1n, 2n, 4n]) // 7n
+   * BitField.summarize([2n, 2n, 4n]) // 6n
+   * BitField.summarize([1n, 4n]) // 5n
+   * BitField.summarize([1n, 1n]) // 1n
+   * BitField.summarize(1n) // 1n
+   * ```
+   */
+  public static summarize(bits: ArrayOrType<Bit>): bigint {
+    if (!Array.isArray(bits)) return BigInt(bits);
+
+    let summ = DEFAULT_BIT;
+    bits.forEach(b => summ |= BigInt(b));
+    return summ;
+  }
+
+  /**
+   * summurize values
+   * 
+   * @example
+   * ```ts
+   * BitField.add(1n, [2n, 4n]) // 7n
+   * BitField.add(2n, [2n, 4n]) // 6n
+   * BitField.add(1n, 4n) // 5n
+   * BitField.add(1n, 1n) // 1n
+   * ```
+   * 
+   * @equals `BitField.add(1n, 2n)` === `Bitiield.summarize([1n, 2n...])`
+   */
   public static add(bit: Bit, add: ArrayOrType<Bit>): bigint {
     return BigInt(bit) | BitField.summarize(add);
   }
   
+  /**
+   * subtract values
+   * 
+   * @example
+   * ```ts
+   * BitField.remove(7n, [4n, 2n]) // 1n
+   * BitField.remove(14n, [4n, 2n]) // 8n
+   * BitField.remove(14n, [4n, 2n, 1n]) // 8n
+   * BitField.remove(14n, [1n]) // 14n
+   * ```
+   */
   public static remove(bit: Bit, remove: ArrayOrType<Bit>): bigint {
     return BigInt(bit) &~ BitField.summarize(remove);
   }
 
+  /**
+   * takes the logarithm of a number
+   * 
+   * @example
+   * ```ts
+   * BitField.logarithm2(1n << 10n) // 10n
+   * BitField.logarithm2(1n << MULTIPLIER) // MULTIPLIER
+   * BitField.logarithm2(2n << 10n) // 11n
+   * BitField.logarithm2(4n << 10n) // 12n
+   * ```
+   */
   public static logarithm2(bigint: Bit): bigint {
     return BigInt(BigInt(bigint).toString(2).length - 1);
   }
 
+  /**
+   * takes max value of values
+   * 
+   * @example
+   * ```ts
+   * BitField.max(1n, 2n, 3n, 4n) // 4n
+   * ``` 
+   */
   public static max(...values: Bit[]): bigint {
     return BigInt(values
       .toSorted((a, b) => {
@@ -28,14 +102,6 @@ class BitField {
             ? -1 : 0;
       }).toReversed()[0]);
   }
-
-  public static summarize(bits: ArrayOrType<Bit>): bigint {
-    if (!Array.isArray(bits)) return BigInt(bits);
-
-    let summ = DEFAULT_BIT;
-    bits.forEach(b => summ |= BigInt(b));
-    return summ;
-  }
   
   public constructor(
     public readonly bits: Bit = DEFAULT_BIT
@@ -43,14 +109,51 @@ class BitField {
     this.bits = BigInt(bits);
   }
 
+  /**
+   * summurize values
+   * 
+   * @example
+   * ```ts
+   * new BitField(1n).add([2n, 4n]) // 7n
+   * new BitField(2n).add([2n, 4n]) // 6n
+   * new BitField(1n).add(4n) // 5n
+   * new BitField(1n).add(1n) // 1n
+   * ```
+   * 
+   * @equals `new BitField(1n).add(2n)` === `Bitiield.summarize([1n, 2n...])`
+   */
   public add(bits: ArrayOrType<Bit>): bigint {
     return BitField.add(this.bits, bits);
   }
 
+  /**
+   * subtract values
+   * 
+   * @example
+   * ```ts
+   * new BitField(7n).remove([4n, 2n]) // 1n
+   * new BitField(14n).remove([4n, 2n]) // 8n
+   * new BitField(14n).remove([4n, 2n, 1n]) // 8n
+   * new BitField(14n).remove([1n]) // 14n
+   * ```
+   */
   public remove(bits: ArrayOrType<Bit>): bigint {
     return BitField.remove(this.bits, bits);
   }
 
+  /**
+   * checking bits equals
+   * 
+   * @example
+   * ```ts
+   * new BitField(1n).has(1n)  // true
+   * new BitField(1n).has("1") // true
+   * new BitField(1n).has(2n)  // false
+   * new BitField(1n).has("2") // false
+   * ```
+   * 
+   * @equals `new BitField(1n).has(2n)` === `BitField.equals(1n, 2n)`
+   */
   public has(bit: ArrayOrType<Bit>): boolean {
     return BitField.equals(
       this.bits,
