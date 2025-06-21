43e828788516219cb9dc573047d61e4dd4ef5d95
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2025-06-21 09:53:22 +0500

    -update compiler.ts

--- src/compiler.ts
+++ src/compiler.ts
@@ -5,35 +5,15 @@ import BitField, { BitBuilder } from "./index";
 import { join, parse } from "path";
 import { existsSync, unlinkSync, writeFileSync } from "fs";
 
-const format = (string: string, capitalize: boolean) =>
-  capitalize
-    ? string.charAt(0).toUpperCase() + string.slice(1)
-    : string.charAt(0).toLowerCase() + string.slice(1);
-
-const formatSettings = <T extends string>(
-  settings: T[] | readonly T[]
-) =>
-  settings.map((string: T) =>
-    format(
-      string
-        .toLowerCase()
-        .replaceAll("__", " ")
-        .replaceAll("_", " ")
-        .split(" ")
-        .map((v) => format(v, true))
-        .join(""),
-      false
-    )
-  );
-
-type ISettings<T extends string> = Record<Capitalize<T>, string[]|readonly string[]>;
+type ISettings<T extends string> = Record<T, string[]|readonly string[]>;
 
 class Compiler<T extends string> {
   public readonly keys: T[];
 
   public constructor(
     public readonly settings: ISettings<T>,
-    public readonly filePath: string
+    public readonly filePath: string,
+    public readonly format: (value: string) => string
   ) {
     this.keys = Object.keys(settings) as T[];
     this.filePath = join(filePath);
@@ -45,12 +25,12 @@ class Compiler<T extends string> {
     this.formatFile();
   }
 
-  public format(type: Capitalize<T>) {
+  public parse(type: T) {
     const settings = this.settings[type];
 
     if (settings.length === 0) return [];
 
-    return formatSettings(settings);
+    return settings.map(string => this.format(string));
   }
 
   private createFile() {
@@ -62,8 +42,7 @@ class Compiler<T extends string> {
   private compile() {
     const settings = Object.fromEntries(
       this.keys.map((key) => [
-        key,
-        this.format(format(key, true) as Capitalize<T>)
+        key, this.parse(key)
       ])
     );
 
